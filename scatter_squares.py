b9fe8285a72c96b1b9387f1386f71ec602224234
irisgli <iris.li@berkeley.edu>
2026-01-18 16:14:39 -0800

    scatter squares

--- scatter_squares.py
+++ scatter_squares.py
@@ -3,7 +3,7 @@ import matplotlib.pyplot as plt
 x_values = list(range(1, 1001))
 y_values = [x**2 for x in x_values]
 
-plt.scatter(x_values, y_values, s=40)
+plt.scatter(x_values, y_values, c =y_values, cmap=plt.cm.Blues, edgecolor = 'none', s=10)
 
 # Set chart title and label axes.
 plt.title("Square Numbers", fontsize=24)
@@ -15,4 +15,5 @@ plt.tick_params(axis='both', which='major', labelsize=14)
 plt.axis([0, 1100, 0, 1100000])
 
 plt.show()
-
+# Saving your plots automatically, (filename, trims extra whitespace from the plot)
+# plt.savefig('square_plot.png', bbox_inches='tight')
