80793f46c35dfdc01529dc7540f09358e8bc6dfd
irisgli <iris.li@berkeley.edu>
2026-01-18 16:28:19 -0800

    colormap

---
+++ rw_visual.py
@@ -0,0 +1,18 @@
+import matplotlib.pyplot as plt
+
+from random_walk import RandomWalk
+
+# Keep making new walks, as long as the program is active.
+while True: 
+    # Make a random walk, and plot the points
+    rw = RandomWalk()
+    rw.fill_walk()
+
+    # Generate list of numbers equal to the number of points in the walk
+    point_numbers = list(range(rw.num_points))
+    plt.scatter(rw.x_values, rw.y_values, c=point_numbers, cmap=plt.cm.Blues, edgecolor='none', s=15)
+    plt.show()
+
+    keep_running = input("Make another walk? (y/n): ")
+    if keep_running == "n":
+        break
